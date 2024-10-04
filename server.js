2ae65d23bd178c245a8f0c54a663dd213b038681
Shinyong Park <stevenpark3222@gmail.com>
2024-10-04 17:16:17 +0900

    process.env.FRONTEND_URL

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000', // frontend URL
+  origin: process.env.FRONTEND_URL, // frontend URL
   credentials: true,
 }));
 
